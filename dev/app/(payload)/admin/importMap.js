d9cb3bc973a47feefe8f9df3c47c8d0c0a8f73d7
sneddonisaac <info.isaacsneddon@gmail.com>
2026-02-11 09:48:48 +0000

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'menu-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'menu-plugin/rsc'
+
+export const importMap = {
+  'menu-plugin/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'menu-plugin/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
